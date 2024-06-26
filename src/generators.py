55eb5c5a14cfc1f76898382de6a5092c2c95ed3a
Vladislav Zorin <zorin.3v6@gmail.com>
2024-06-26 12:36:26 +0300

    Add function transaction_descriptions

--- src/generators.py
+++ src/generators.py
@@ -10,3 +10,13 @@ def filter_by_currency(transactions: list[dict], code: str) -> Iterator[dict]:
     :return: Итератор, выдающий по очереди операции, в которых указана заданная валюта.
     """
     return (transaction for transaction in transactions if transaction["operationAmount"]["currency"]["code"] == code)
+
+
+def transaction_descriptions(transactions: list[dict]) -> Iterator[str]:
+    """
+    Генератор, возвращающий описание каждой операции по очереди.
+
+    :param transactions: Список операций.
+    :return: Генератор, который поочередно возвращает описания операций из каждого словаря в списке transactions.
+    """
+    return (transaction["description"] for transaction in transactions)
