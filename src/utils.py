2213a4a464aae620fd76200d482de2553f1cb447
Vladislav Zorin <zorin.3v6@gmail.com>
2024-07-15 14:29:52 +0300

    Update utils.py

--- src/utils.py
+++ src/utils.py
@@ -2,6 +2,11 @@ import json
 import os
 from typing import Any, Dict, List
 
+from src.logger_config import setup_logger
+
+# Создание и получение именованного логгера
+utils_logger = setup_logger(__name__)
+
 
 def read_transactions_json(file_path: str) -> List[Dict[str, Any]]:
     """
@@ -11,15 +16,19 @@ def read_transactions_json(file_path: str) -> List[Dict[str, Any]]:
     :return: Список транзакций.
     """
     if not os.path.exists(file_path):
+        utils_logger.warning(f"File {file_path} does not exist.")
         return []
 
     try:
         with open(file_path) as file:
             data = json.load(file)
             if isinstance(data, list):
+                utils_logger.info(f"Successfully read file: {file_path}")
                 return data
             else:
+                utils_logger.warning(f"Invalid data format in file: {file_path}")
                 return []
 
-    except (json.JSONDecodeError, IOError):
+    except (json.JSONDecodeError, IOError) as e:
+        utils_logger.error(f"Error reading file {file_path}: {e}")
         return []
